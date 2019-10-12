bbe972d6aaa902950a2db34ea58d525c35d579a4
hcelante <higor.celante@gmail.com>
2019-10-12 17:14:38 -0300

    correcao eq e commments main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
-import src.equivalent as eq
+import src.equivalent as eq # reponsavel por buscar os estados equivalentes
  
 
 def main():
-    with open((sys.argv[1]), "r") as f:
-        auxList1 = [line.strip().split(" ") for line in f]
+    with open((sys.argv[1]), "r") as f: # recebe o arquivo por parametro
+        auxList1 = [line.strip().split(" ") for line in f] # armazena em uma lista
     if auxList1 == None:
         print("\nNao foi possivel carregar o LFA!\nExecute o codigo novamente passando o txt correto por parametro.\n")
     else:
         print("Procurando estados equivalentes...")
         alfabeto = auxList1[0]
-        Equivalent = eq.table_create(auxList1[7:], alfabeto)
+        Equivalent = eq.table_create(auxList1[3], alfabeto)
         if Equivalent == None:
             print("Nao foram encontrados estados equivalentes!")
         else:
--- src/equivalent.py
+++ src/equivalent.py
@@ -1,10 +1,10 @@
-def table_create(lista, alfabeto):
+def table_create(lista, alfabeto):  # Cria a tabela de estados 
     equiv = []
     
-    for n in lista[:(len(lista)-1)]:
-        for i in lista.reverse[1:]:
-            for a in alfabeto:
-                equiv.append(equivalent_match(n,i,a))
+    for n in lista[:(len(lista)-1)]: # Ex: 0, 1, 2, 3, 4 menos o ultimo
+        for i in lista.reverse[1:]:  # Ex: 1, 2, 3, 4, 5 menos o primeiro
+            for a in alfabeto:       # testa para cada letra
+                equiv.append(equivalent_match(n,i,a)) # n= estado1 , i= estado 2, a= letra
 
 
     if True:
