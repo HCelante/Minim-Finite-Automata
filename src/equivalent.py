de78cdc50d17276ae651a5b7af758ba709fae31e
hcelante <higor.celante@gmail.com>
2019-10-12 17:49:50 -0300

    correcao eq

--- src/equivalent.py
+++ src/equivalent.py
@@ -1,17 +1,12 @@
-def table_create(lista, alfabeto):  # Cria a tabela de estados 
+def table_create(lista, alfabeto, transicoes):  # Cria a tabela de estados 
     equiv = []
     
     for n in lista[:(len(lista)-1)]: # Ex: 0, 1, 2, 3, 4 menos o ultimo
         for i in lista.reverse[1:]:  # Ex: 1, 2, 3, 4, 5 menos o primeiro
             for a in alfabeto:       # testa para cada letra
-                equiv.append(equivalent_match(n,i,a)) # n= estado1 , i= estado 2, a= letra
+                equiv.append(equivalent_match(n,i,a,transicoes)) # n= estado1 , i= estado 2, a= letra
+    return equiv
 
 
-    if True:
-        return equiv
-    else:
-        return None
-
-
-def equivalent_match(sx,sy,signal): # retorna se equivalent
+def equivalent_match(sx,sy,signal,transicoes): # retorna se equivalent
     pass
